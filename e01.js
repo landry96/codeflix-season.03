055bf81506062b151d5bcec8c3a964fc9e0a0efc
landry96 <55443238+landry96@users.noreply.github.com>
2020-11-16 11:16:54 +0100

    http server node

---
+++ e01.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const PORT = process.argv[2]
+
+// Check if PORT defined
+if (!PORT) {
+  console.log('usage: node e01.js <PORT>')
+  process.exit(0)
+}
+
+// Execute HTTP Server
+const server = http.createServer((_, res) => {
+    res.setHeader("Content-Type", "text/html")
+    res.write('<h1> Hello World!</h1>')
+    res.end()
+})
+
+server.listen(PORT, () => {
+  console.log(`server started at  http://localhost:${PORT}`)
+})
